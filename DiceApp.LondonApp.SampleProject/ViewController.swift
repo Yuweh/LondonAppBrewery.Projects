8d6d01c1149647fb0b453b2909cb9f0ef9c7dec8
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-12 22:52:38 +0800

    Initial Commit

---
+++ DiceApp.LondonApp.SampleProject/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DiceApp.LondonApp.SampleProject
+//
+//  Created by Francis Jemuel Bergonia on 12/07/2017.
+//  Copyright © 2017 Francis Jemuel Bergonia. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
