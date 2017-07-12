fb1f2503f7851675d3fefdcd5487608534025cae
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-12 23:26:52 +0800

    1st Phase

--- DiceApp.LondonApp.SampleProject/ViewController.swift
+++ DiceApp.LondonApp.SampleProject/ViewController.swift
@@ -10,6 +10,15 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    var randomDiceIndex1 : Int = 0
+    var randomDiceIndex2 : Int = 0
+    
+    @IBOutlet weak var diceImageView1: UIImageView!
+    @IBOutlet weak var diceImageView2: UIImageView!
+    
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -21,5 +30,11 @@ class ViewController: UIViewController {
     }
 
 
+    @IBAction func rollButtonPressed(_ sender: Any) {
+        
+        randomDiceIndex1 = Int(arc4random(6))
+        randomDiceIndex2 = Int(arc4random(6))
+        
+    }
 }
 
