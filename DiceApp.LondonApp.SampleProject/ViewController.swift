4aa03fc4d4b32d51545fb722b0ad7eda8b211957
Francis Jemuel Bergonia <francisyuweh@Franciss-MacBook-Pro.local>
2017-07-13 20:56:08 +0800

    With Arrays and arc4random :D

--- DiceApp.LondonApp.SampleProject/ViewController.swift
+++ DiceApp.LondonApp.SampleProject/ViewController.swift
@@ -14,6 +14,8 @@ class ViewController: UIViewController {
     var randomDiceIndex1 : Int = 0
     var randomDiceIndex2 : Int = 0
     
+    let diceArray = ["dice1", "dice2", "dice3", "dice4", "dice5", "dice6"]
+    
     @IBOutlet weak var diceImageView1: UIImageView!
     @IBOutlet weak var diceImageView2: UIImageView!
     
@@ -32,8 +34,11 @@ class ViewController: UIViewController {
 
     @IBAction func rollButtonPressed(_ sender: Any) {
         
-        randomDiceIndex1 = Int(arc4random(6))
-        randomDiceIndex2 = Int(arc4random(6))
+        randomDiceIndex1 = Int(arc4random_uniform(6))
+        randomDiceIndex2 = Int(arc4random_uniform(6))
+     
+        diceImageView1.image = UIImage(named: diceArray[randomDiceIndex1])
+        diceImageView2.image = UIImage(named: diceArray[randomDiceIndex2])
         
     }
 }
